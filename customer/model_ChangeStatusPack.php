7c61e0db0f67e67849e9c8a0a96552afad3f093e
sampanmen <sampan.sara@outlook.com>
2015-09-23 11:53:38 +0700

    แก้งานให้นิว

--- customer/model_ChangeStatusPack.php
+++ customer/model_ChangeStatusPack.php
@@ -1,61 +1,48 @@
-<form>
-
-    <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"><b>Changes Status</b></h4>
-    </div>
-
-
-
-    <div class="modal-body">
-        <div>
-            <div class="col-lg-12">  
-                <div class="col-lg-5">
-                    <p><b>Order ID:</b></p>      
-                </div>
-                <div class="col-lg-7">                               
-                    00001                               
-                </div>
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <h4 class="modal-title" id="gridSystemModalLabel">Modal title</h4>
+</div>
+<div class="modal-body">
+    <div class="container-fluid">
+        <div class="col-lg-12">  
+            <div class="col-lg-5">
+                <p><b>Order ID:</b></p>      
             </div>
-            <div class="col-lg-12">  
-                <div class="col-lg-5">
-                    <p><b>Package:</b></p>      
-                </div>
-                <div class="col-lg-7">                               
-                    Full Rack                          
-                </div>
+            <div class="col-lg-7">                               
+                00001                               
             </div>
-            <div class="col-lg-12">  
-                <div class="col-lg-5">
-                    <p><b>Type:</b></p>      
-                </div>
-                <div class="col-lg-7">                               
-                    Main Packages                         
-                </div>
+        </div>
+        <div class="col-lg-12">  
+            <div class="col-lg-5">
+                <p><b>Package:</b></p>      
             </div>
-            <div class="col-lg-12">  
-                <div class="col-lg-5">
-                    <p><b>Status</b></p>      
-                </div>
-                <div class="col-lg-7">                               
-                    <select>
-                        <option>Active</option>
-                        <option>Susspend</option> 
-                        <option>No Active</option>
-                    </select>                              
-                </div>
+            <div class="col-lg-7">                               
+                Full Rack                          
+            </div>
+        </div>
+        <div class="col-lg-12">  
+            <div class="col-lg-5">
+                <p><b>Type:</b></p>      
+            </div>
+            <div class="col-lg-7">                               
+                Main Packages                         
+            </div>
+        </div>
+        <div class="col-lg-12">  
+            <div class="col-lg-5">
+                <p><b>Status</b></p>      
+            </div>
+            <div class="col-lg-7">                               
+                <select>
+                    <option>Active</option>
+                    <option>Susspend</option> 
+                    <option>No Active</option>
+                </select> 
             </div>
         </div>
     </div>
-
-
-
-
-
-    
-        <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-    
-</form>
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+    <button type="button" class="btn btn-primary">Save changes</button>
+</div>
