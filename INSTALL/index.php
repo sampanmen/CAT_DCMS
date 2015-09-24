b99f3102278f680587e52a61bb0c43fe7f4ad908
sampanmen <sampan.sara@outlook.com>
2015-09-24 15:07:37 +0700

    Add INSTALL
    auto add Database

---
+++ INSTALL/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$user = "root";
+$pass = "123456";
+$dbh = new PDO('mysql:host=localhost', $user, $pass);
+$sql = file_get_contents('MySQL_install.sql');
+$qr = $dbh->exec($sql);
+
+if ($dbh->errorInfo()[0]=="0") {
+    echo "INSTALL COMPLETED.";
+} else {
+    echo "INSTALL FAILED.";
+    echo "<pre>";
+    print_r($dbh->errorInfo());
+    echo "</pre>";
+}
