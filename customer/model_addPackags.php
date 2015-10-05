9b930a596449fa2f3e616d934f1e68e894decc55
thidanews <thidarat.changkaew@gmail.com>
2015-10-05 13:43:30 +0700

    button save

--- customer/model_addPackags.php
+++ customer/model_addPackags.php
@@ -66,5 +66,5 @@
 </div>
 <div class="modal-footer">
     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-    <button type="button" class="btn btn-primary">Save changes</button>
+    <button type="button" class="btn btn-primary">Save</button>
 </div>
