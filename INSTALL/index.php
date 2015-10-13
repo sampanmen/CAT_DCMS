2b743ced81107f7e24e982cd374c1331bd448e69
sampanmen <sampan.sara@outlook.com>
2015-10-13 16:07:50 +0700

    edit INSTALL

--- INSTALL/index.php
+++ INSTALL/index.php
@@ -3,7 +3,7 @@
 $user = "cat";
 $pass = "123456";
 $dbh = new PDO('mysql:host=localhost', $user, $pass);
-$qr = $dbh->exec("DROP DATABASE `cat_dcms`;");
+//$qr = $dbh->exec("DROP DATABASE `cat_dcms`;");
 $qr = $dbh->exec("SET NAMES UTF8");
 $sql = file_get_contents('MySQL_install.sql');
 $qr = $dbh->exec($sql);
