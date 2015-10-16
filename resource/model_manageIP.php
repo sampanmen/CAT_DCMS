e3eadb9085f75e2b3466d06bddaf1475989c601d
thidanews <thidarat.changkaew@gmail.com>
2015-10-16 12:35:52 +0700

    manage IP

--- resource/model_manageIP.php
+++ resource/model_manageIP.php
@@ -4,16 +4,78 @@
 </div>
 
 
-<div class="container-fluid">
-    <div class="col-lg-5">
-        <div class="form-group ">
-            <br><p>Network</p>
-            <select class="form-control" name="cus[bussinessType]" id="cusBissinessType">
-                <option>192.168.1.0 (10)</option>
-                <option>192.168.2.0 (110)</option>
-                <option>192.168.3.0 (30)</option>
-            </select>    
-        </div>
+<div class="container-fluid"><br>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h6><b>IP Address Used</b></h6>
+        </div>      
+
+
+
+        <div class="panel-body">
+            <div class="dataTable_wrapper">
+                <table class="table  table-bordered " id="dataTables">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>IP Address</th>
+                            <th>Network</th>
+                            <th>Subnet</th>
+                            <th>Vlan</th>
+                            <th>Customer</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><button type="button" class="btn btn-danger btn-circle "><i class="glyphicon-minus"></i></button></td>
+                            <td>192.168.1.1</td>
+                            <td>192.168.1.0</td>
+                            <td>/24</td>
+                            <td>1</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><button type="button" class="btn btn-danger btn-circle"><i class="glyphicon-minus"></i></button></td>
+                            <td>192.168.1.2</td>
+                            <td>192.168.1.0</td>
+                            <td>/24</td>
+                            <td>1</td>
+                            <td>vdghjkjl;</td>
+                        </tr>
+                        <tr>
+                            <td><button type="button" class="btn btn-danger btn-circle "><i class="glyphicon-minus"></i></button></td>
+                            <td>192.168.1.3</td>
+                            <td>192.168.1.0</td>
+                            <td>/24</td>
+                            <td>1</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>   
+    </div>
+
+
+
+
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h6><b>Select</b></h6>
+        </div>  
+        <br>
+        <div class="col-lg-12">
+            <div class=" col-lg-2">
+                <p>Network</p>
+            </div>
+            <div class=" col-lg-6">
+                <select  name="cus[bussinessType]" id="cusBissinessType">
+                    <option>192.168.1.0 (10)</option>
+                    <option>192.168.2.0 (110)</option>
+                    <option>192.168.3.0 (30)</option>
+                </select>    
+            </div>
         </div>
 
 
@@ -33,7 +95,7 @@
                     </thead>
                     <tbody>
                         <tr>
-                            <td><input type="checkbox"></td>
+                            <td><button type="button" class="btn btn-success btn-circle"><i class="glyphicon-plus"></i></button></td>
                             <td>192.168.1.1</td>
                             <td>192.168.1.0</td>
                             <td>/24</td>
@@ -41,7 +103,7 @@
                             <td></td>
                         </tr>
                         <tr>
-                            <td><input type="checkbox"></td>
+                            <td><button type="button" class="btn btn-success btn-circle "><i class="glyphicon-plus"></i></button></td>
                             <td>192.168.1.2</td>
                             <td>192.168.1.0</td>
                             <td>/24</td>
@@ -49,7 +111,7 @@
                             <td>vdghjkjl;</td>
                         </tr>
                         <tr>
-                            <td><input type="checkbox"></td>
+                            <td><button type="button" class="btn btn-success btn-circle "><i class="glyphicon-plus"></i></button></td>
                             <td>192.168.1.3</td>
                             <td>192.168.1.0</td>
                             <td>/24</td>
@@ -62,6 +124,7 @@
         </div>
     </div>
 </div>
+
 
 
 
