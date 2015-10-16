ff9bbee7f079dce8171e299cae01caa7ed4012f6
thidanews <thidarat.changkaew@gmail.com>
2015-10-16 12:53:19 +0700

    manage Port

--- resource/model_managePort.php
+++ resource/model_managePort.php
@@ -4,78 +4,164 @@
 </div>
 
 <div class="container-fluid">
-    <div class="col-lg-5">
-        <div class="form-group ">
-            <br><p>Switch</p>
-            <select class="form-control">
-                <option>Switch1 192.168.1.1 (5)</option>
-                <option>Switch1 192.168.2.1 (24)</option>
-                <option>Switch1 192.168.3.1 (6)</option>
-            </select>    
-        </div>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h6><b>Port Used</b></h6>
+        </div>      
+        <div class="panel-body">
+            <div class="dataTable_wrapper">
+                <table class="table table-striped table-bordered table-hover" id="dataTables">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Switch</th>
+                            <th>Port</th>
+                            <th>Vlan</th>
+                            <th>Type</th>
+                            <th>Customer</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><button type="button" class="btn btn-danger btn-circle "><i class="glyphicon-minus"></i></td>
+                            <td>Switch 1</td>
+                            <td>1</td>
+                            <td>100</td>
+                            <td>Fast</td>
+                            <td>HaHa</td>
+                        </tr>
+                        <tr>
+                            <td><button type="button" class="btn btn-danger btn-circle "><i class="glyphicon-minus"></i></td>
+                            <td>Switch 1</td>
+                            <td>2</td>
+                            <td>100</td>
+                            <td>Fast</td>
+                            <td>HaHa</td>
+                        </tr>
+                        <tr>
+                            <td><button type="button" class="btn btn-danger btn-circle "><i class="glyphicon-minus"></i></td>
+                            <td>Switch 1</td>
+                            <td>3</td>
+                            <td>100</td>
+                            <td>Fast</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><button type="button" class="btn btn-danger btn-circle "><i class="glyphicon-minus"></i></td>
+                            <td>Switch 1</td>
+                            <td>4</td>
+                            <td>100</td>
+                            <td>Fast</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><button type="button" class="btn btn-danger btn-circle "><i class="glyphicon-minus"></i></td>
+                            <td>Switch 1</td>
+                            <td>5</td>
+                            <td>100</td>
+                            <td>Fast</td>
+                            <td>jjjjjjjjj</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>   
     </div>
 
 
-    <div class="panel-body">
-        <div class="dataTable_wrapper">
-            <table class="table table-striped table-bordered table-hover" id="dataTables">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Switch</th>
-                        <th>Port</th>
-                        <th>Vlan</th>
-                        <th>Type</th>
-                        <th>Customer</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><input type="checkbox"></td>
-                        <td>Switch 1</td>
-                        <td>1</td>
-                        <td>100</td>
-                        <td>Fast</td>
-                        <td>HaHa</td>
-                    </tr>
-                    <tr>
-                        <td><input type="checkbox"></td>
-                        <td>Switch 1</td>
-                        <td>2</td>
-                        <td>100</td>
-                        <td>Fast</td>
-                        <td>HaHa</td>
-                    </tr>
-                    <tr>
-                        <td><input type="checkbox"></td>
-                        <td>Switch 1</td>
-                        <td>3</td>
-                        <td>100</td>
-                        <td>Fast</td>
-                        <td></td>
-                    </tr>
-                     <tr>
-                        <td><input type="checkbox"></td>
-                        <td>Switch 1</td>
-                        <td>4</td>
-                        <td>100</td>
-                        <td>Fast</td>
-                        <td></td>
-                    </tr>
-                     <tr>
-                        <td><input type="checkbox"></td>
-                        <td>Switch 1</td>
-                        <td>5</td>
-                        <td>100</td>
-                        <td>Fast</td>
-                        <td>jjjjjjjjj</td>
-                    </tr>
-                </tbody>
-            </table>
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h6><b>Select</b></h6>
+        </div>  
+        <br>
+        <div class="col-lg-12">
+            <div class=" col-lg-2">
+                <p>Switch</p>
+            </div>
+            <div class=" col-lg-6">
+                <select >
+                    <option>Switch1 192.168.1.1 (5)</option>
+                    <option>Switch1 192.168.2.1 (24)</option>
+                    <option>Switch1 192.168.3.1 (6)</option>
+                </select>    
+            </div>
+        </div>
+
+
+        <div class="panel-body">
+            <div class="dataTable_wrapper">
+                <table class="table table-striped table-bordered table-hover" id="dataTables">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Switch</th>
+                            <th>Port</th>
+                            <th>Vlan</th>
+                            <th>Type</th>
+                            <th>Customer</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><button type="button" class="btn btn-success btn-circle"><i class="glyphicon-plus"></i></button></td>
+                            <td>Switch 1</td>
+                            <td>1</td>
+                            <td>100</td>
+                            <td>Fast</td>
+                            <td>HaHa</td>
+                        </tr>
+                        <tr>
+                            <td><button type="button" class="btn btn-success btn-circle"><i class="glyphicon-plus"></i></button></td>
+                            <td>Switch 1</td>
+                            <td>2</td>
+                            <td>100</td>
+                            <td>Fast</td>
+                            <td>HaHa</td>
+                        </tr>
+                        <tr>
+                            <td><button type="button" class="btn btn-success btn-circle"><i class="glyphicon-plus"></i></button></td>
+                            <td>Switch 1</td>
+                            <td>3</td>
+                            <td>100</td>
+                            <td>Fast</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><button type="button" class="btn btn-success btn-circle"><i class="glyphicon-plus"></i></button></td>
+                            <td>Switch 1</td>
+                            <td>4</td>
+                            <td>100</td>
+                            <td>Fast</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><button type="button" class="btn btn-success btn-circle"><i class="glyphicon-plus"></i></button></td>
+                            <td>Switch 1</td>
+                            <td>5</td>
+                            <td>100</td>
+                            <td>Fast</td>
+                            <td>jjjjjjjjj</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
-</div>
+
 
 
 
