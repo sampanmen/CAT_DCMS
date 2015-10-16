2d5a77447fdb4be3cb0bedb87f8c50420fa4622d
sampanmen <sampan.sara@outlook.com>
2015-10-16 11:14:31 +0700

    Revert "Revert "a little change""
    This reverts commit 87200c185e4f6ab87dd83b2b7d74f66e4b8419b8.

--- customer/model_addPackags.php
+++ customer/model_addPackags.php
@@ -34,6 +34,7 @@
                                 <option value="shared rack">Shared Rack</option>
                                 <option value="firewall">Firewall</option>
                                 <option value="room">Room</option>
+                                <option value="add-on">Add on</option>
                             </select>   
                         </div>
                         <div class="form-group col-lg-6">
