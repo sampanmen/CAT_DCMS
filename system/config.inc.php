bb5d89af27464328472b9b0f7aef5c92b24cda8e
sampanmen <sampan.sara@outlook.com>
2015-10-21 11:14:54 +0700

    Optimization Func.

--- system/config.inc.php
+++ system/config.inc.php
@@ -1,8 +1,16 @@
 <?php
+
 // config database
 define("DSN", 'mysql:host=localhost;dbname=cat_dcms');
 define("DBUSER", 'cat');
 define("DBPASS", '123456');
 $connection;
 
-//
+function dbconnect() {
+    global $connection;
+    if (!$connection) {
+        $connection = new PDO(DSN, DBUSER, DBPASS);
+        $connection->query("SET NAMES UTF8");
+    }
+    return $connection;
+}
--- system/function.inc.php
+++ system/function.inc.php
@@ -1,5 +1,5 @@
 <?php
 require_once dirname(__FILE__).'/config.inc.php';
-require_once dirname(__FILE__).'/functionDB.inc.php';
+require_once dirname(__FILE__).'/customer.func.inc.php';
 require_once dirname(__FILE__).'/resource.func.inc.php';
 require_once dirname(__FILE__).'/entryIDC.func.inc.php';
