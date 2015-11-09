26a072974ce692e2a38c2935ee7a16919168070b
thidanews <thidarat.changkaew@gmail.com>
2015-11-09 09:44:42 +0700

    creatfile

---
+++ admin/action/admin.action.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../system/function.inc.php';
+
+$para = isset($_GET['para']) ? $_GET['para'] : "";
+$personID = "-1";
+
+if ($para == "addEntryIDC") {
+    
+} else if ($para == "checkOut") {
+    
+}
--- system/function.inc.php
+++ system/function.inc.php
@@ -3,3 +3,4 @@ require_once dirname(__FILE__).'/config.inc.php';
 require_once dirname(__FILE__).'/customer.func.inc.php';
 require_once dirname(__FILE__).'/resource.func.inc.php';
 require_once dirname(__FILE__).'/entryIDC.func.inc.php';
+require_once dirname(__FILE__).'/admin.func.inc.php';
