247f6ed21419d53c7168c891a365886156893245
sampanmen <sampan.sara@outlook.com>
2015-11-19 10:53:07 +0700

    fix Show IP by Location

--- resource/viewIP.php
+++ resource/viewIP.php
@@ -6,38 +6,39 @@ $getIPs = getIPs($networkID);
 ?>
 <p><a href="?">Home</a> > <b>IP Address</b></p>
 <div class="row">
-    <div class="col-lg-3"> 
+    <div class="col-lg-3">
+
         <div class="panel panel-default">
             <div class="panel-heading">
                 <h5><b>Network IP </b><a href="../resource/model_addIP.php" data-toggle="modal" data-target="#myModal">(Add)</a></h5>
             </div>      
 
             <div class="panel-body">
-                <div class="dataTable_wrapper">
-                    <table class="table table-striped table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <th>Network</th>
-                                <th>Vlan</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $getNetworks = getNetworks();
-                            foreach ($getNetworks as $value) {
-                                $valNetworkID = $value['NetworkID'];
-                                $valNetwork = $value['NetworkIP'];
-                                $valSubnet = $value['Subnet'];
-                                $valVlan = $value['Vlan'];
-                                $valStatus = $value['Status'];
-                                ?>
-                                <tr <?php echo $networkID == $valNetworkID ? 'class="active"' : ""; ?>>
-                                    <td><a href="?p=viewIP&NetworkID=<?php echo $valNetworkID; ?>" class="text-<?php echo $valStatus == "Active" ? "success" : "danger"; ?>"><?php echo $valNetwork; ?> / <?php echo $valSubnet; ?></a></td>
-                                    <td><?php echo $valVlan; ?></td>
-                                </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                <div class="form-group">
+                    <select class="form-control" name="location" id="location" onchange="showNetwork();">
+                        <option value="">Choose location</option>
+                        <?php
+                        $getLocationID = $_GET['LocationID'];
+                        $getLocation = getLocation();
+                        foreach ($getLocation as $value) {
+                            $valLocationID = $value['LocationID'];
+                            $valLocation = $value['Location'];
+                            ?>
+                            <option <?php echo $valLocationID == $getLocationID ? "selected" : ""; ?> value="<?php echo $valLocationID; ?>"><?php echo $valLocation; ?></option>
+                        <?php } ?>
+                    </select>
+                    <script>
+                        showNetwork();
+                        function showNetwork() {
+                            var getLocation = $("#location").val();
+                            $.get("../resource/action/resource.content.showIP.php?LocationID=" + getLocation, function (data, status) {
+                                $("#showNetwork").html(data);
+                            });
+                        }
+                    </script>
+                </div>
+                <div class="dataTable_wrapper" id="showNetwork">
+                    <!--show IP here-->
                 </div>
                 <!-- /.table-responsive -->
             </div>
