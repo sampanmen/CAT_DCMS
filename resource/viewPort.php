c76333fdba080ef8cdd9754b2e9e5ad80316bd2f
sampanmen <sampan.sara@outlook.com>
2015-11-23 20:30:01 +0700

    fix View Resource

--- resource/viewPort.php
+++ resource/viewPort.php
@@ -72,13 +72,14 @@ $swID = (isset($_GET['swID'])) ? $_GET['swID'] : "";
                                 $valCustomerID = $value['CustomerID'];
                                 $valCustomerName = $value['CustomerName'];
                                 $valUplink = $value['Uplink'];
+                                $valStatusUsed = $value['StatusUsed'];
                                 ?>
                                 <tr>
                                     <td><?php echo $valSwitchName; ?></td>
                                     <td><?php echo $valPortNumber; ?></td>
                                     <td><?php echo $valPortType; ?></td>
                                     <td>
-                                        <?php echo ($valUplink == 1) ? "Uplink" : ($valCustomerName == NULL ? "NULL" : "<a target='_blank' href='?p=viewCus&cusID=" . $valCustomerID . "'>" . $valCustomerName . "</a>"); ?>
+                                        <?php echo ($valUplink == 1) ? "Uplink" : (($valCustomerName == NULL || $valStatusUsed == "Deactive") ? "NULL" : "<a target='_blank' href='?p=viewCus&cusID=" . $valCustomerID . "'>" . $valCustomerName . "</a>"); ?>
                                     </td>
                                 </tr> 
                             <?php } ?>
